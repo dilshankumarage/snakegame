19165fc97330ab05a99b91c98e91475a6d5df071
Dilshan Chathuranga Kumarage <140938017+dilshankumarage@users.noreply.github.com>
2025-06-27 17:37:47 +0300

    Update scoreboard.py
    Updated with storing and retrieving highest score

--- scoreboard.py
+++ scoreboard.py
@@ -1,36 +1,37 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = ("Arial", 24, "normal")
+FONT = ("Courier", 24, "normal")
+
 class Scoreboard(Turtle):
+
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = self.load_highscore()
         self.color("white")
         self.penup()
+        self.goto(0, 270)
         self.hideturtle()
-        self.goto(0, 290)  # Position at top of screen
-        self.update_score()
-
-    def update_score(self):
+        self.update_scoreboard()
+    @staticmethod
+    def load_highscore():
+        try:
+            with open("data.txt") as file:
+                return int(file.read())
+        except (FileNotFoundError, ValueError):
+            return 0
+    def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score {self.highscore}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.highscore))
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.update_score()
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("Arial", 30, "bold"))
-
-def draw_border():
-    border = Turtle()
-    border.hideturtle()
-    border.penup()
-    border.pensize(3)
-    border.color("white")
-    border.goto(-290, 290)
-    border.pendown()
-    for _ in range(4):
-        border.forward(580)
-        border.right(90)
+        self.update_scoreboard()
