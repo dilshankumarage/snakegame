bc2611f1b012041785c259428317773327d7d7aa
Dilshan Chathuranga Kumarage <140938017+dilshankumarage@users.noreply.github.com>
2025-06-27 17:37:11 +0300

    Update main.py
    Updated with retrieving and storing highest score

--- main.py
+++ main.py
@@ -1,57 +1,51 @@
-from turtle import  Screen
-import time
+from turtle import Screen
 from snake import Snake
 from food import Food
-from scoreboard import Scoreboard, draw_border
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-paused = False
 
 snake = Snake()
 food = Food()
-score = Scoreboard()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
-
-def toggle_pause():
-    global paused
-    paused = not paused
-screen.onkey(toggle_pause, "space")
-
-
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    if not paused:
-        snake.move_snake()
-    head_x = snake.head.xcor()
-    head_y = snake.head.ycor()
-    draw_border()
+    snake.move()
 
-    #Detect collision with Food
+    #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        score.increase_score()
+        scoreboard.increase_score()
 
-    #Detect collision with wall
-    if head_x > 280 or head_x < -280 or head_y > 280 or head_y < -280:
-        game_is_on = False
-        score.game_over()
-    #Detect collision with tail
-    for segment in snake.snake[1:]:  # Skip the head (snake.snake[0])
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+
+
+
+
 
 screen.exitonclick()
